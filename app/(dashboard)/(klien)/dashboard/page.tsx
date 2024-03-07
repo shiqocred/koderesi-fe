718e6211fa6ad16632d199d6f399b7ef44d3acef
shiqocred <21103080090@student.uin-suka.ac.id>
2024-03-07 12:02:21 +0700

    card resi

--- app/(dashboard)/(klien)/dashboard/page.tsx
+++ app/(dashboard)/(klien)/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { Plus, Rocket } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const data = [
@@ -10,6 +11,7 @@ const data = [
     kode_resi: "SPX010001020102",
     keterangan: "resi sepatu",
     kode_kurir: "JNE Cargo",
+    status: "on progress",
     shipper: { nama: "ahmad fulan", origin: "jakarta" },
     receiver: { nama: "azizi", destination: "surabaya" },
     last_manifest: {
@@ -23,6 +25,7 @@ const data = [
     kode_resi: "SPX3200202323",
     keterangan: "resi televisi",
     kode_kurir: "shopee express",
+    status: "on progress",
     shipper: { nama: "ahmad fulan", origin: "jakarta" },
     receiver: { nama: "azizi", destination: "surabaya" },
     last_manifest: {
@@ -36,6 +39,7 @@ const data = [
     kode_resi: "JT02291201219",
     keterangan: "resi pakaian",
     kode_kurir: "J&T",
+    status: "on progress",
     shipper: { nama: "ahmad fulan", origin: "jakarta" },
     receiver: { nama: "azizi", destination: "surabaya" },
     last_manifest: {
@@ -49,6 +53,7 @@ const data = [
     kode_resi: "SPX3200202323",
     keterangan: "resi televisi",
     kode_kurir: "shopee express",
+    status: "delivered",
     shipper: { nama: "ahmad fulan", origin: "jakarta" },
     receiver: { nama: "azizi", destination: "surabaya" },
     last_manifest: {
@@ -62,6 +67,7 @@ const data = [
     kode_resi: "JT02291201219",
     keterangan: "resi pakaian",
     kode_kurir: "J&T",
+    status: "delivered",
     shipper: { nama: "ahmad fulan", origin: "jakarta" },
     receiver: { nama: "azizi", destination: "surabaya" },
     last_manifest: {
@@ -80,7 +86,9 @@ const DashboardPage = () => {
         <Card className="w-full px-7 py-4 flex justify-between items-center">
           <div>
             <h3 className="text-xl font-bold">Lacak Resi Anda!</h3>
-            <p className="text-sm text-gray-500">Silahkan masukan resi anda</p>
+            <p className="text-sm text-gray-500 dark:text-gray-300 font-light">
+              Silahkan masukan resi anda
+            </p>
           </div>
           <Button
             className="w-16 h-16 border border-green-400 rounded-md text-green-400 hover:text-green-400 hover:bg-gray-50 dark:bg-gray-900 dark:hover:bg-gray-800"
@@ -92,7 +100,7 @@ const DashboardPage = () => {
         <Card className="w-full px-7 py-4 flex justify-between items-center">
           <div>
             <h3 className="text-xl font-bold">Kredit Habis?</h3>
-            <p className="text-sm text-gray-500">
+            <p className="text-sm text-gray-500 dark:text-gray-300 font-light">
               Top up kredit anda sekarang juga!
             </p>
           </div>
@@ -103,12 +111,14 @@ const DashboardPage = () => {
               </h3>
               <p className="text-xs text-white">Total kredit anda</p>
             </div>
-            <Button
-              className="w-16 h-16 border border-green-400 rounded-md text-green-400 hover:text-green-400 hover:bg-gray-50 dark:bg-gray-900 dark:hover:bg-gray-800"
-              variant={"outline"}
-            >
-              <Rocket className="w-6 h-6" />
-            </Button>
+            <Link href={"/top-up"}>
+              <Button
+                className="w-16 h-16 border border-green-400 rounded-md text-green-400 hover:text-green-400 hover:bg-gray-50 dark:bg-gray-900 dark:hover:bg-gray-800"
+                variant={"outline"}
+              >
+                <Rocket className="w-6 h-6" />
+              </Button>
+            </Link>
           </div>
         </Card>
       </div>
@@ -117,24 +127,26 @@ const DashboardPage = () => {
         <div className="w-3/12 gap-4 flex flex-col">
           <Card className="w-full h-16 items-center flex px-6 justify-between">
             <p>Total Resi</p>
-            <p className="font-bold text-lg">5</p>
+            <p className="font-bold">5</p>
           </Card>
           <Card className="w-full h-16 items-center flex px-6 justify-between">
             <p>On Progress</p>
-            <p className="font-bold text-lg">5</p>
+            <p className="font-bold">5</p>
           </Card>
           <Card className="w-full h-16 items-center flex px-6 justify-between">
             <p>Delivered</p>
-            <p className="font-bold text-lg">5</p>
+            <p className="font-bold">5</p>
           </Card>
         </div>
-        <Card className="w-9/12 p-4 rounded-md space-y-4">
+        <Card className="w-9/12 p-4 rounded-md flex flex-col gap-y-4">
           {data.map((item) => (
-            <ResiCard key={item.kode_resi} {...item}></ResiCard>
+            <ResiCard key={item.kode_resi} {...item} isDashboard></ResiCard>
           ))}
-          <Button className="bg-green-400 hover:bg-green-500 text-gray-900">
-            Lebih banyak...
-          </Button>
+          <Link href="/tracks">
+            <Button className="bg-green-400 hover:bg-green-500 text-gray-900 text-xs">
+              Lebih banyak...
+            </Button>
+          </Link>
         </Card>
       </div>
     </div>
--- components/resi-card.tsx
+++ components/resi-card.tsx
@@ -1,11 +1,14 @@
 "use client";
+import { ArrowDown, MoreHorizontal } from "lucide-react";
 import { Card } from "./ui/card";
 import { Separator } from "./ui/separator";
+import { cn } from "@/lib/utils";
 
 interface ResiCardProps {
   kode_resi: string;
   keterangan: string;
   kode_kurir: string;
+  status: string;
   shipper: { nama: string; origin: string };
   receiver: { nama: string; destination: string };
   last_manifest?: {
@@ -14,6 +17,7 @@ interface ResiCardProps {
     date: string;
     waktu: string;
   };
+  isDashboard?: boolean;
 }
 
 export const ResiCard = ({
@@ -23,6 +27,8 @@ export const ResiCard = ({
   shipper,
   receiver,
   last_manifest,
+  status,
+  isDashboard,
 }: ResiCardProps) => {
   return (
     <Card className="border border-green-200 p-4 space-y-4 text-sm">
@@ -33,22 +39,43 @@ export const ResiCard = ({
         <div className="flex gap-16">
           <div>
             <h3>{kode_resi}</h3>
-            <p className="text-xs text-gray-500">{kode_kurir}</p>
+            <p className="text-xs text-gray-500 dark:text-gray-300">
+              {kode_kurir}
+            </p>
           </div>
-          <div>
-            <p>{shipper.nama + " (" + shipper.origin + ")"}</p>
-            <Separator className="bg-gray-500" />
-            <p>{receiver.nama + " (" + receiver.destination + ")"}</p>
+          <div className="relative flex flex-col justify-center">
+            <p className="pr-2 capitalize">
+              {shipper.nama + " (" + shipper.origin + ")"}
+            </p>
+            <Separator className="bg-gray-300 dark:bg-gray-400" />
+            <p className="pr-2 capitalize">
+              {receiver.nama + " (" + receiver.destination + ")"}
+            </p>
+            <div className="text-gray-500 absolute -right-4 dark:text-gray-300">
+              <ArrowDown className="w-3 h-3" />
+            </div>
           </div>
-          <div>
-            <div>{last_manifest?.manifest + " " + last_manifest?.city}</div>
-            <Separator className="bg-gray-500" />
-            <div>{last_manifest?.date + " - " + last_manifest?.waktu}</div>
-          </div>
+          {!isDashboard && (
+            <div>
+              <div>{last_manifest?.manifest + " " + last_manifest?.city}</div>
+              <Separator className="bg-gray-300" />
+              <div>{last_manifest?.date + " - " + last_manifest?.waktu}</div>
+            </div>
+          )}
         </div>
-        <div className="flex">
-          <div>status</div>
-          <div>more</div>
+        <div className="flex gap-x-4">
+          <div
+            className={cn(
+              "w-28 h-8 flex items-center justify-center rounded capitalize text-sm text-gray-900",
+              status === "on progress" && "bg-yellow-300",
+              status === "delivered" && "bg-green-300"
+            )}
+          >
+            {status}
+          </div>
+          <div className="h-8 w-8 rounded-full border-gray-300 bg-gray-100 dark:bg-gray-700 dark:text-gray-300 text-gray-500 flex items-center justify-center">
+            <MoreHorizontal className="h-4 w-4" />
+          </div>
         </div>
       </div>
     </Card>
