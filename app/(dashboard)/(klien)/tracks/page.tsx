83975cf243e3b11f012450d2aeafbd0b22766d39
shiqocred <21103080090@student.uin-suka.ac.id>
2024-03-11 08:23:40 +0700

    fix csr

--- app/(dashboard)/(klien)/tracks/page.tsx
+++ app/(dashboard)/(klien)/tracks/page.tsx
@@ -6,7 +6,7 @@ import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { cn, data } from "@/lib/utils";
 import { LayoutGrid, LayoutList, Search } from "lucide-react";
-import React, { useCallback, useState } from "react";
+import React, { Suspense, useCallback, useState } from "react";
 import { ResiCard } from "@/components/resi-card";
 import { useRouter, useSearchParams } from "next/navigation";
 import qs from "query-string";
@@ -104,72 +104,78 @@ const TracksPage = () => {
         <div className="flex gap-4 w-full flex-col lg:flex-row">
           <div className="w-full relative flex items-center">
             <Search className="w-5 h-5 peer absolute left-3 text-gray-500" />
-            <Input
-              value={search}
-              onChange={(e) => handleChange(e.target.value)}
-              className="pl-10 peer-hover:border-green-400 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-ring focus-visible:ring-offset-0 border-green-200 focus-visible:border-green-400 placeholder:text-gray-500 hover:border-green-400 dark:border-green-200/40 dark:focus-visible:border-green-400 dark:hover:border-green-400"
-              placeholder="Search resi code..."
-            />
+            <Suspense>
+              <Input
+                value={search}
+                onChange={(e) => handleChange(e.target.value)}
+                className="pl-10 peer-hover:border-green-400 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-ring focus-visible:ring-offset-0 border-green-200 focus-visible:border-green-400 placeholder:text-gray-500 hover:border-green-400 dark:border-green-200/40 dark:focus-visible:border-green-400 dark:hover:border-green-400"
+                placeholder="Search resi code..."
+              />
+            </Suspense>
           </div>
           <div className="flex lg:w-auto w-full gap-4">
             <div className="flex w-full lg:w-auto border-green-200 dark:border-green-200/40 border rounded-md hover:border-green-400 dark:hover:border-green-400">
-              <Button
-                onClick={() => handleClickSort("semua")}
-                className={cn(
-                  "w-full lg:w-[100px] text-gray-900 text-xs dark:text-white",
-                  filter === "semua"
-                    ? "bg-green-100 hover:bg-green-200 dark:bg-gray-700 dark:hover:bg-gray-600"
-                    : "bg-transparent hover:bg-green-100 dark:hover:bg-gray-700"
-                )}
-              >
-                Semua
-              </Button>
-              <Button
-                onClick={() => handleClickSort("on_progress")}
-                className={cn(
-                  "w-full lg:w-[100px] text-gray-900 text-xs dark:text-white",
-                  filter === "on_progress"
-                    ? "bg-green-100 hover:bg-green-200 dark:bg-gray-700 dark:hover:bg-gray-600"
-                    : "bg-transparent hover:bg-green-100 dark:hover:bg-gray-700"
-                )}
-              >
-                On Progress
-              </Button>
-              <Button
-                onClick={() => handleClickSort("delivered")}
-                className={cn(
-                  "w-full lg:w-[100px] text-gray-900 text-xs dark:text-white",
-                  filter === "delivered"
-                    ? "bg-green-100 hover:bg-green-200 dark:bg-gray-700 dark:hover:bg-gray-600"
-                    : "bg-transparent hover:bg-green-100 dark:hover:bg-gray-700"
-                )}
-              >
-                Delivered
-              </Button>
+              <Suspense>
+                <Button
+                  onClick={() => handleClickSort("semua")}
+                  className={cn(
+                    "w-full lg:w-[100px] text-gray-900 text-xs dark:text-white",
+                    filter === "semua"
+                      ? "bg-green-100 hover:bg-green-200 dark:bg-gray-700 dark:hover:bg-gray-600"
+                      : "bg-transparent hover:bg-green-100 dark:hover:bg-gray-700"
+                  )}
+                >
+                  Semua
+                </Button>
+                <Button
+                  onClick={() => handleClickSort("on_progress")}
+                  className={cn(
+                    "w-full lg:w-[100px] text-gray-900 text-xs dark:text-white",
+                    filter === "on_progress"
+                      ? "bg-green-100 hover:bg-green-200 dark:bg-gray-700 dark:hover:bg-gray-600"
+                      : "bg-transparent hover:bg-green-100 dark:hover:bg-gray-700"
+                  )}
+                >
+                  On Progress
+                </Button>
+                <Button
+                  onClick={() => handleClickSort("delivered")}
+                  className={cn(
+                    "w-full lg:w-[100px] text-gray-900 text-xs dark:text-white",
+                    filter === "delivered"
+                      ? "bg-green-100 hover:bg-green-200 dark:bg-gray-700 dark:hover:bg-gray-600"
+                      : "bg-transparent hover:bg-green-100 dark:hover:bg-gray-700"
+                  )}
+                >
+                  Delivered
+                </Button>
+              </Suspense>
             </div>
             <div className="md:flex hidden border-green-200 border rounded-md hover:border-green-400">
-              <Button
-                onClick={() => handleClickLayout("list")}
-                className={cn(
-                  "w-10 text-xs p-0",
-                  layout === "list"
-                    ? "bg-green-100 hover:bg-green-200 dark:bg-gray-700 dark:hover:bg-gray-600"
-                    : "bg-transparent hover:bg-green-100 dark:hover:bg-gray-700"
-                )}
-              >
-                <LayoutList className="w-4 h-4 text-gray-900 dark:text-white" />
-              </Button>
-              <Button
-                onClick={() => handleClickLayout("grid")}
-                className={cn(
-                  "w-10 text-xs p-0",
-                  layout === "grid"
-                    ? "bg-green-100 hover:bg-green-200 dark:bg-gray-700 dark:hover:bg-gray-600"
-                    : "bg-transparent hover:bg-green-100 dark:hover:bg-gray-700"
-                )}
-              >
-                <LayoutGrid className="w-4 h-4 text-gray-900 dark:text-white" />
-              </Button>
+              <Suspense>
+                <Button
+                  onClick={() => handleClickLayout("list")}
+                  className={cn(
+                    "w-10 text-xs p-0",
+                    layout === "list"
+                      ? "bg-green-100 hover:bg-green-200 dark:bg-gray-700 dark:hover:bg-gray-600"
+                      : "bg-transparent hover:bg-green-100 dark:hover:bg-gray-700"
+                  )}
+                >
+                  <LayoutList className="w-4 h-4 text-gray-900 dark:text-white" />
+                </Button>
+                <Button
+                  onClick={() => handleClickLayout("grid")}
+                  className={cn(
+                    "w-10 text-xs p-0",
+                    layout === "grid"
+                      ? "bg-green-100 hover:bg-green-200 dark:bg-gray-700 dark:hover:bg-gray-600"
+                      : "bg-transparent hover:bg-green-100 dark:hover:bg-gray-700"
+                  )}
+                >
+                  <LayoutGrid className="w-4 h-4 text-gray-900 dark:text-white" />
+                </Button>
+              </Suspense>
             </div>
           </div>
         </div>
