7d24f4e73f8ac4f6846e16d7bcc340415650d869
shiqocred <21103080090@student.uin-suka.ac.id>
2024-03-15 16:27:27 +0700

    add profile

--- components/header.tsx
+++ components/header.tsx
@@ -2,22 +2,25 @@
 
 interface HeaderProps {
   title: string;
-  description: string;
+  description?: string;
+  isAccount?: boolean;
 }
 
-export const Header = ({ title, description }: HeaderProps) => {
+export const Header = ({ title, description, isAccount }: HeaderProps) => {
   return (
     <div className="flex w-full justify-between md:items-center border-b border-gray-500 pb-4 md:flex-row flex-col items-start gap-4">
       <div className="flex flex-col">
         <h3 className="xl:text-xl text-lg font-bold">{title}</h3>
         <p className="font-light text-sm lg:text-base">{description}</p>
       </div>
-      <div className="px-6 w-full md:w-auto flex flex-row items-center justify-between md:flex-col md:items-start bg-green-400 h-10 md:h-[50px] md:justify-center rounded-md">
-        <p className="text-sm text-gray-900 leading-none">Kredit anda</p>
-        <h3 className="font-bold text-sm lg:text-lg leading-none dark:text-gray-900">
-          3500 kredit
-        </h3>
-      </div>
+      {!isAccount && (
+        <div className="px-6 w-full md:w-auto flex flex-row items-center justify-between md:flex-col md:items-start bg-green-400 h-10 md:h-[50px] md:justify-center rounded-md">
+          <p className="text-sm text-gray-900 leading-none">Kredit anda</p>
+          <h3 className="font-bold text-sm lg:text-lg leading-none dark:text-gray-900">
+            3500 kredit
+          </h3>
+        </div>
+      )}
     </div>
   );
 };
