a7786931fb433553d0c3f1048dd4edc4444fae8f
shiqocred <21103080090@student.uin-suka.ac.id>
2024-04-27 19:45:12 +0700

    fix

--- components/navbar/float-navbar.tsx
+++ components/navbar/float-navbar.tsx
@@ -54,7 +54,10 @@ export const FloatNavbar = ({
         transition={{
           duration: 0.2,
         }}
-        className={cn("flex w-screen fixed top-5 z-50 left-0 px-6", className)}
+        className={cn(
+          "flex w-screen fixed top-5 z-50 left-0 px-4 sm:px-6",
+          className
+        )}
       >
         <div className="flex w-full justify-between items-center inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-md dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[20] px-2 py-1.5 space-x-4">
           <IsiNavbar open={open} onOpenChange={onOpenChange} />
