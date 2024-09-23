91e4bafd17143dd47368334608e7bb3dd0193140
shiqocred <21103080090@student.uin-suka.ac.id>
2024-09-23 21:27:25 +0700

    add

--- components/modals/edit-reason-modal.tsx
+++ components/modals/edit-reason-modal.tsx
@@ -59,21 +59,24 @@ export const EditReasonModal = () => {
       isOpen={isModalOpen}
       onClose={onClose}
     >
-      <div className="flex gap-2 w-full">
+      <form onSubmit={onApprove} className="w-full flex flex-col gap-4">
         <p>Apakah anda yakin ingin mengubah alasan tersebut?</p>
-        <Button
-          onClick={onClose}
-          className="w-full dark:text-white border border-green-300 hover:border-green-400 dark:border-green-700/70 dark:hover:border-green-700 bg-transparent hover:bg-transparent text-black"
-        >
-          Batal
-        </Button>
-        <Button
-          onClick={onApprove}
-          className="w-full bg-green-400 hover:bg-green-300 text-black"
-        >
-          Approve
-        </Button>
-      </div>
+        <div className="flex gap-2 w-full">
+          <Button
+            onClick={onClose}
+            type="button"
+            className="w-full dark:text-white border border-green-300 hover:border-green-400 dark:border-green-700/70 dark:hover:border-green-700 bg-transparent hover:bg-transparent text-black"
+          >
+            Batal
+          </Button>
+          <Button
+            type="submit"
+            className="w-full bg-green-400 hover:bg-green-300 text-black"
+          >
+            Approve
+          </Button>
+        </div>
+      </form>
     </Modal>
   );
 };
